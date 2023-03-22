d06fdd2a14085242784f78c5f2abc6cf785c4ebd
Rodney <123950191+rodneyd05@users.noreply.github.com>
2023-03-22 20:37:05 +0800

    Added name and city

--- app/src/main/java/com/thisisit/employmentui/MainActivity.kt
+++ app/src/main/java/com/thisisit/employmentui/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.thisisit.employmentui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import com.thisisit.employmentui.databinding.ActivityMainBinding
 
@@ -13,6 +16,128 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
+
+        //applying for
+        mainBinding.positionApplyingForEdittext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputPosition = mainBinding.positionApplyingForEdittext.text.toString()
+
+                mainBinding.positionTextView.text = inputPosition
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        //Date Available
+        mainBinding.dateAvailableForWorkEdittext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputDate = mainBinding.dateAvailableForWorkEdittext.text.toString()
+
+                mainBinding.dateAvailableTextView.text = inputDate
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        //Desired Salary
+        mainBinding.desiredSalaryEdittext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputSalary = mainBinding.desiredSalaryEdittext.text.toString()
+
+                mainBinding.salaryTextView.text = "P$inputSalary"
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        //First Name
+        mainBinding.firstName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputFirstName = mainBinding.firstName.text.toString()
+
+                mainBinding.firstNameTextView.text = inputFirstName
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        //Last Name
+        mainBinding.lastName.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            @SuppressLint("SetTextI18n")
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputLastName = mainBinding.lastName.text.toString()
+
+                mainBinding.lastNameTextView.text = " $inputLastName"
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+
+        //City
+        mainBinding.city.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                val inputCity = mainBinding.city.text.toString()
+
+                mainBinding.cityTextView.text = inputCity
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
     }
 
     @Suppress("UNUSED_PARAMETER")
@@ -70,7 +195,7 @@ class MainActivity : AppCompatActivity() {
             mainBinding.referencesContent.visibility = View.VISIBLE
             mainBinding.arrow4.setImageResource(R.drawable.arrow_down)
         } else {
-            mainBinding.references.visibility = View.GONE
+            mainBinding.referencesContent.visibility = View.GONE
             mainBinding.arrow4.setImageResource(R.drawable.arrow_right)
         }
     }
